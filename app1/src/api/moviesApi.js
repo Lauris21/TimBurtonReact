d9124348686746e3202c62e8f07a1a7e406e25dd
Lauris-21 <laurasanchez0301@gmail.com>
2023-01-06 22:48:44 +0100

    Fetch

--- app1/src/api/moviesApi.js
+++ app1/src/api/moviesApi.js
@@ -1,6 +1,7 @@
-
 export const getMovies = async () => {
-    const data = await fetch("http://localhost:8080/TimBurtonMovies/")
-    const dataToJson = await data.json();
-    return dataToJson;
-}
+  const data = await fetch(
+    "https://timburtonblog.onrender.com/TimBurtonMovies"
+  );
+  const dataToJson = await data.json();
+  return dataToJson;
+};
