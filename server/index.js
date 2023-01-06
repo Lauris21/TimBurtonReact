141040760e0c58d8af9a0f6db6dbadc1ae5314c7
Lauris-21 <laurasanchez0301@gmail.com>
2023-01-06 22:17:00 +0100

    icon

--- server/index.js
+++ server/index.js
@@ -6,5 +6,5 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 server.listen(8080, () => {
-  console.log("JSON Server is running");
+  console.log("JSON Server is running 🚀");
 });
